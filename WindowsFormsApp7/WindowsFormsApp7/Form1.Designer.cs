cd6e4071cdecd5faf8d357aa63168b79d17789ed
VeronikaRomanenko <Veronika.Romanenko333@gmail.com>
2019-10-30 21:55:11 +0200

    commi

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Созданные потоки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ожидающие потоки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Потоки в работе";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Создать поток";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 160);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(178, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(160, 160);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(344, 39);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(160, 160);
+            this.listBox3.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 229);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 261);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        Semaphore s;
+        List<Thread> threads1;
+        List<Thread> threads2;
+        List<Thread> threads3;
+        int counter;
+        public Form1()
+        {
+            InitializeComponent();
+           
+            s = new Semaphore(2, 2);
+            threads1 = new List<Thread>();
+            threads2 = new List<Thread>();
+            threads3 = new List<Thread>();
+            counter = 0;
+            numericUpDown1.Value = 2;
+        }
+
+        private void UpdateListBox(object listBox, object tmp, bool isAdded)
+        {
+            ListBox list = listBox as ListBox;
+            if (list.InvokeRequired)
+            {
+                list.Invoke(new Action<object, object, bool>(UpdateListBox), listBox, tmp, isAdded);
+            }
+            else
+            {
+                if (isAdded)
+                {
+                    list.Items.Add(tmp);
+                }
+                else
+                    list.Items.RemoveAt((int)tmp);
+            }
+        }
+
+        private void GoToWork()
+        {
+            if (counter < numericUpDown1.Value && threads2.Count > 0)
+            {
+                Thread t = threads2[0];
+                threads2.Remove(t);
+                UpdateListBox(listBox2 as object, 0, false);
+                threads3.Add(t);
+                UpdateListBox(listBox3 as object, t.ManagedThreadId, true);
+                counter++;
+                t.Start();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(() => ThreadMethod());
+            threads1.Add(t);
+            listBox1.Items.Add(t.ManagedThreadId);
+         
+        }
+
+        private void ThreadMethod()
+        {
+            if (s.WaitOne())
+            {
+                Thread.Sleep(5000);
+                s.Release();
+                threads3.RemoveAt(0);
+                UpdateListBox(listBox3 as object, 0, false);
+                counter--;
+              
+            }  GoToWork();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            GoToWork();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            Thread t = threads1[listBox1.SelectedIndex];
+            threads2.Add(t);
+            listBox2.Items.Add(t.ManagedThreadId);
+            threads1.Remove(t);
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            GoToWork();
+        }
+    }
+}
